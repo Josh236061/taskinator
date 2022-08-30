72d6bb533aa85fca1aea3b1834061c1a05ebcb84
Josh M Thomas <71394743+Josh236061@users.noreply.github.com>
2022-08-30 14:45:13 -0500

    Added 'Handle Form Submission' element(s)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
     event.preventDefault();
-    
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", createTaskHandler);
